863534c7a0436ff6f99e6428457ba7b223d4a774
Breslavets Maria <alwayslearn@mail.ru>
2025-11-04 20:48:49 +0300

    feat: solve isMAC48Address and sortByHeight

--- src/sort-by-height.js
+++ src/sort-by-height.js
@@ -11,9 +11,13 @@ const { NotImplementedError } = require('../lib');
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-function sortByHeight(/* arr */) {
-  // Remove line below and write your code here
-  throw new NotImplementedError('Not implemented');
+function sortByHeight(arr) {
+  const myArr = arr.filter((num) => num !== -1).sort((a, b) => a - b);
+  let index = 0
+  return arr.map((num) => {
+    if (num === -1) return -1;
+    return myArr[index++];
+  });
 }
 
 module.exports = {
